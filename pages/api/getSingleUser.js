c7290074dfa6ed6a5d00880b523547c3af974b53
Joseph Adewole <Josephadewole1@gmail.com>
2022-06-13 03:14:40 +0100

    Update getSingleUser.js

--- pages/api/getSingleUser.js
+++ pages/api/getSingleUser.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 module.exports = async(req, res) => {
-    const { id } = req.query;
+    const { id } = 1;
     const listofUsers = (await axios.get('https://jsonplaceholder.typicode.com/users')).data;
     const singleUser = listofUsers.filter(item => {
         if(item.id == id) return item
