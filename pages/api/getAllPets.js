07df09e64e11da3eda57968fd81e868d647d3218
Joseph Adewole <Josephadewole1@gmail.com>
2022-06-13 04:16:43 +0100

    Update getAllPets.js

--- pages/api/getAllPets.js
+++ pages/api/getAllPets.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 module.exports = async(req, res) => {
     console.log('got here')
+    const { id } = req.query;
     const listofPets = (await axios.get('http://petstore.execute-api.us-east-1.amazonaws.com/petstore/pets')).data;
     console.log('got response')
-    return res.json(listofPets[0])
+    const singlePet = listofUsers.filter(item => {
+        if(item.id == id) return item
+    });
+    return res.json(singlePet[0])
 }
